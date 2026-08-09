7966a49af7e86ce796b3fe31efa3d42d8303e657
Andrew Naplavkov <andrew.naplavkov@gmail.com>
2026-08-09 10:17:24 +0300

    map_view_async

--- example/ugis/map_view.cpp
+++ example/ugis/map_view.cpp
@@ -6,7 +6,6 @@
 #include <QWheelEvent>
 #include <boat/gui/qt.hpp>
 #include <boost/gil.hpp>
-#include "catalog.h"
 #include "map_view.h"
 
 namespace {
@@ -63,73 +62,6 @@ void map_view::resizeEvent(QResizeEvent*)
 {
     update();
     schedule_redraw();
-}
-
-void map_view::redraw()
-{
-    tasks_.request_stop();
-    auto w = width(), h = height();
-    if (w <= 0 || h <= 0)
-        return;
-    auto mid = map_mid_;
-    auto scale = map_scale_;
-    auto crs = geo::ortho(mid);
-    auto mat = geo::affine(
-        w, h, pixel(mid, scale, fwd_transform(geo::transformation(crs))));
-    auto num_points = static_cast<size_t>(
-        (w * h) / (boat::tile::size * boat::tile::size) + 4);
-    tasks_.run([=, lyrs = layers_](auto tok) {
-        auto catalogs =
-            std::map<std::string, std::unique_ptr<boat::db::catalog>>{};
-        auto grid = geo::geographic_interpolate(w, h, mat, crs, num_points);
-        auto img = QImage{w, h, QImage::Format_RGBA8888};
-        img.fill(Qt::white);
-        auto art = QPainter{&img};
-        art.setRenderHint(QPainter::Antialiasing);
-        art.setCompositionMode(QPainter::CompositionMode_Darken);
-        auto drw = boat::gui::draw_variant(std::execution::seq, art, mat, crs);
-        for (auto& l : lyrs) {
-            if (tok.stop_requested())
-                return;
-            try {
-                if (!l.layer.raster) {
-                    art.setPen(l.pen);
-                    art.setBrush(l.brush);
-                }
-                auto pvd = boat::gui::provider{
-                    .catalog = [&] -> boat::db::catalog& {
-                        auto& cat = catalogs[l.address];
-                        if (!cat)
-                            cat = make_catalog(l.address);
-                        return *cat;
-                    },
-                    .layer = l.layer,
-                    .cache = cache_,
-                    .key = l.cache,
-                    .grid = grid,
-                };
-                for (auto var : pvd.variants()) {
-                    if (tok.stop_requested())
-                        return;
-                    std::visit(drw, var);
-                }
-            }
-            catch (std::exception const& e) {
-                qWarning() << "draw error:" << e.what();
-            }
-        }
-        QMetaObject::invokeMethod(
-            this,
-            [=, img = std::move(img)] mutable {
-                if (tok.stop_requested())
-                    return;
-                img_ = std::move(img);
-                img_mid_ = mid;
-                img_scale_ = scale;
-                update();
-            },
-            Qt::QueuedConnection);
-    });
 }
 
 void map_view::paintEvent(QPaintEvent*)
